620acdd2ecbded9d8d0d1d3e2284976b31934712
Katt3A <00001254328634sp@al.educacao.sp.gov.br>
2024-11-25 21:48:22 -0300

    Add files via upload

---
+++ v.1 - Katt @/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cultura Pop',
+    'Mascote da Franquia Pokemon?',
+    'Pikachu'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'Nome do Protagonista de Jujutsu Kaisen?',
+    'sator--- Yuji Itadori'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'Na franquia Dragon Ball existem quantas esfera do dragão?',
+    'existem 7 esferas'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'sistema de poder usado em Naruto?',
+    'Chakra'
+)
